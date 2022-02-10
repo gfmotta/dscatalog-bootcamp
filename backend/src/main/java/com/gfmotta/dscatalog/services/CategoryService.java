40608fc7d2ad03ddceb7f1f87bc11a630d8fe23e
Gustavo Motta <guforatini@gmail.com>
2022-02-09 23:08:02 -0300

    Find category by id and exception handling

--- backend/src/main/java/com/gfmotta/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gfmotta/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.gfmotta.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +11,24 @@ import org.springframework.transaction.annotation.Transactional;
 import com.gfmotta.dscatalog.dto.CategoryDTO;
 import com.gfmotta.dscatalog.entities.Category;
 import com.gfmotta.dscatalog.repositories.CategoryRepository;
+import com.gfmotta.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> categoryList = repository.findAll();
-		return categoryList.stream()
-				.map(x -> new CategoryDTO(x))
-				.collect(Collectors.toList());		
+		return categoryList.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category category = obj.orElseThrow(() -> new EntityNotFoundException("Departamento não encontrado"));
+		return new CategoryDTO(category);
 	}
 }
