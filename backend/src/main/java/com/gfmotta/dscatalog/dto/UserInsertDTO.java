0951f0ccf87a77b31594875c586711f134aa49a7
Gustavo Motta <guforatini@gmail.com>
2022-03-30 13:08:50 -0300

    Custom constraint validator

--- backend/src/main/java/com/gfmotta/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/gfmotta/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.gfmotta.dscatalog.dto;
 
+import com.gfmotta.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
---
+++ backend/src/main/java/com/gfmotta/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,43 @@
+package com.gfmotta.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gfmotta.dscatalog.dto.UserInsertDTO;
+import com.gfmotta.dscatalog.entities.User;
+import com.gfmotta.dscatalog.repositories.UserRepository;
+import com.gfmotta.dscatalog.resources.exceptions.FieldMessage;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = repository.findByEmail(dto.getEmail());
+		
+		if (user != null) {
+			list.add(new FieldMessage("email", "Esse email ja esta em uso"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
